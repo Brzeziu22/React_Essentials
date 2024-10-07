f7672b624370af12146cdec238f78132752f7c85
Brzeziu22 <kamilb79@gmail.com>
2024-10-07 20:33:34 +0200

    First functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+function Header(){ //creating a function component
+  return ( <header> {/* () means that fuction is going to return more then one line of code */}
+    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <h1>React Essentials</h1>
+    <p>
+      Fundamental React concepts you will need for almost any app you are
+      going to build!
+    </p>
+  </header>)
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header /> {/* Using our function componten*/}
       <main>
         <h2>Time to get started!</h2>
       </main>
