4b27501567e53f60db121bf6ce88c854b1efc5a0
Brzeziu22 <kamilb79@gmail.com>
2024-11-04 20:31:38 +0100

    first step to displaying dynamic content in components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcept/CoreConcept';
+import TabButton from './components/Button/Button';
+
 import componentImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data';
 
@@ -9,6 +11,9 @@ import { CORE_CONCEPTS } from './data';
 
 
 function App() {
+  function handleClick(selectedButton){
+    console.log(selectedButton)
+}
   return (
     <div>
       <Header /> {/* Using our function component*/}
@@ -20,7 +25,15 @@ function App() {
         {CORE_CONCEPTS.map(item => <CoreConcept {...item}/>)} {/*Works if props and item have similar value names*/}
         </ul>
         </section>
-        <h2>Time to get started!</h2>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onClick={handleClick}>Components</TabButton> {/* passing function to child component without any arguments so we can add arguments in chlid component*/}
+            <TabButton onClick={handleClick}>JSX</TabButton>
+            <TabButton onClick={handleClick}>Props</TabButton>
+            <TabButton onClick={handleClick}>State</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,3 +1,10 @@
-export default Button =({children})=>{
-    return <button>{children}</button>
+
+
+
+const TabButton =({children, onClick})=>{
+
+   
+
+    return <button onClick={()=>{onClick(children)}}>{children}</button> ;{/**{/* Adding value passed to child component as children props as parent component function argument*/}
 }
+export default TabButton
